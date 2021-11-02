37e63f7e3af46c48d895829390d2772b0595873a
robiardian <robiardian1987@gmail.com>
2021-11-02 12:18:07 +0700

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MDPL Prak Pertemuan ke 6</title>
+</head>
+<body>
+    <h2>materi pertemuan ke 6 </h2>
+</body>
+</html>
